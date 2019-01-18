57ca9be7e9d54e2ff733b733b14e77bf0eb16757
Galimsky Artur <motorokz@gmail.com>
2019-01-18 15:05:25 +0500

    решена проблема некорректной очистки Controls - Controls.Clear()

---
+++ WinForms/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WinForms
+{
+    //Form functionality here
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateLabels();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DestroyLabels();
+        }
+    }
+    public partial class Form1 : Form
+    {
+        private void CreateLabels()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Label helloButton = new Label();
+                helloButton.Location = new Point(30, 30 + i * 30);
+                helloButton.Text = i.ToString();
+                Controls.Add(helloButton);
+            }
+        }
+
+        private void DestroyLabels()
+        {
+            Controls.Clear();
+        }
+    }
+}
