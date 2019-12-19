8cdf2ced7662a76d9f5a1ce4d57a3bfc904c393f
OOORTUR\slownerd <motorokz@gmail.com>
2019-12-19 06:29:22 +0500

    strategy

--- Patterns_Strategy/Program.cs
+++ Patterns_Strategy/Program.cs
@@ -10,8 +10,37 @@ namespace Patterns_Strategy
     {
         static void Main(string[] args)
         {
+            Client client = new Client();
             Console.WriteLine("Strategy Pattern");
+            client.Run(new ConcreteA());
+            client.Run(new ConcreteB());
             Console.ReadLine();
         }
+
+        interface IStrategy { string GetMessage(); }
+        class ConcreteA : IStrategy
+        {
+            public string GetMessage()
+            {
+                return "New message";
+            }
+        }
+        class ConcreteB : IStrategy
+        {
+            public string GetMessage()
+            {
+                return "Message 2";
+            }
+        }
+        class Client
+        {
+            public Client() { }
+            public void Run(IStrategy strategy)
+            {
+                Console.WriteLine($"Message is:  {strategy.GetMessage()}");
+            }
+        }
+
+        
     }
 }
