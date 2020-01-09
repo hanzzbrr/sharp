0dc08dbf089466a4032d6adcf66be3b03e1c6ed6
ooortur <galimsky.artur@primetals.com>
2020-01-09 18:31:14 +0500

    threading, tasks

---
+++ CLRThreading/TaskResultExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLRThreading
+{
+    class TaskResultExample
+    {
+        public TaskResultExample()
+        {
+            Task<Int32> t = new Task<int>(n => Sum((Int32)n, "task0"), 500);
+            t.Start();
+            Task<Int32> t1 = new Task<int>(n => Sum((Int32)n, "task1"), 200);
+            
+            t1.Start();
+            Task.WaitAny();
+            //t.Wait();
+            //t1.Wait();
+            Console.WriteLine("The sum is " + t.Result);
+            Console.WriteLine("The sum is " + t1.Result);
+            Console.ReadLine();
+        }
+
+        private Int32 Sum(Int32 n, string name)
+        {
+            Int32 sum = 0;
+            for (; n > 0; n--)
+            {
+                checked { sum += n; }
+                Console.WriteLine(name +" : "+ sum);
+            }
+            return sum;
+        }
+    }
+}
---
+++ IEnumeratorNumerable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumeratorNumerable
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerator enumerator = "beer".GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var element = enumerator.Current;
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+        }
+    }
+}
