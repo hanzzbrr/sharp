8886245ac11d05c252501d1d7a31b573a18c7051
galimskyy <motorokz@gmail.com>
2020-06-26 00:59:23 +0500

    linked list

---
+++ LinkedList/Doubly/DoublyLinkedList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Doubly
+{
+    public class ObjectCell
+    {
+        public object Value;
+        public ObjectCell Next;
+        public ObjectCell Previous;
+
+        public ObjectCell(object value)
+        {
+            Value = value;
+        }
+    }
+
+    public class DoublyLinkedList
+    {
+        public ObjectCell Top;
+        public ObjectCell End;
+
+        public DoublyLinkedList()
+        {
+            Top = new ObjectCell(null);            
+            End = new ObjectCell(null);
+
+            End.Next = null;
+            End.Previous = Top;
+            Top.Next = End;
+            Top.Previous = null;
+        }
+
+        public void AddCellAtStart(ObjectCell newCell)
+        {
+            InsertCell(Top, newCell);
+        }
+        public void AddCellAtEnd(ObjectCell newCell)
+        {
+            InsertCell(End.Previous, newCell);
+        }
+
+        public void InsertCell(ObjectCell afterCell, ObjectCell newCell)
+        {
+            newCell.Next = afterCell.Next;
+            afterCell.Next = newCell;
+
+            newCell.Next.Previous = newCell;
+            newCell.Previous = afterCell;
+        }
+
+        public void IterateFromTop()
+        {
+            ObjectCell top = Top;
+            while (top != null)
+            {
+                Console.WriteLine(top.Value);
+                top = top.Next;
+            }
+        }
+        public void IterateFromEnd()
+        {
+            ObjectCell end = End;
+
+            while (end != null)
+            {
+                Console.WriteLine(end.Value);
+                end = end.Previous;
+            }
+        }
+    }
+
+    public class DoublyLinkedListExample
+    {
+        public DoublyLinkedListExample()
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+            doublyLinkedList.AddCellAtStart(new ObjectCell("Some new value"));
+            doublyLinkedList.AddCellAtStart(new ObjectCell("Another new value"));
+            doublyLinkedList.AddCellAtEnd(new ObjectCell("and one more value"));
+
+            doublyLinkedList.IterateFromTop();
+        }
+    }
+}
---
+++ LinkedList/Singly/SinglyLinkedList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Singly
+{
+    class ObjectCell
+    {
+        public object Value;
+        public ObjectCell Next;
+
+        public ObjectCell(object value)
+        {
+            this.Value = value;
+        }
+    }
+
+
+    class SinglyLinkedListIterator
+    {
+        public void Iterate(ObjectCell top)
+        {
+            while (top != null)
+            {
+                Console.WriteLine(top.Value);
+                top = top.Next;
+            }
+            Console.WriteLine("End of list\n");
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public ObjectCell Top;
+
+        public SinglyLinkedList()
+        {
+            Top = new ObjectCell(null);
+            Top.Next = null;
+        }
+
+        public void AddAtBeginning(ObjectCell newCell)
+        {
+            newCell.Next = Top.Next;
+            Top.Next = newCell;
+        }
+        public void AddAtEnd(ObjectCell newCell)
+        {
+            while (Top.Next != null)
+            {
+                Top = Top.Next;
+            }
+            Top.Next = newCell;
+            newCell.Next = null;
+        }
+        public ObjectCell FindCell(object value)
+        {
+            ObjectCell top = Top;
+            while (top != null)
+            {
+                if (top.Value == value) return top;
+                top = top.Next;
+            }
+            return null;
+        }
+        public void InsertCell(ObjectCell afterCell, ObjectCell newCell)
+        {
+            newCell.Next = afterCell.Next;
+            afterCell.Next = newCell;
+        }
+        public void DeleteAfter(ObjectCell afterCell)
+        {
+            afterCell.Next = afterCell.Next.Next;
+        }
+    }
+
+    class SinglyLinkedListExample
+    {
+        public SinglyLinkedListExample()
+        {
+            var singlyLinkedList = new SinglyLinkedList();
+            singlyLinkedList.AddAtEnd(new ObjectCell("some value"));
+            singlyLinkedList.AddAtEnd(new ObjectCell("some value1"));
+            singlyLinkedList.AddAtBeginning(new ObjectCell("some Valeu 2"));
+
+            var iterator = new SinglyLinkedListIterator();
+            iterator.Iterate(singlyLinkedList.Top);
+
+            ObjectCell foundCell = singlyLinkedList.FindCell("some value"); //O(N)
+
+            singlyLinkedList.InsertCell(foundCell, new ObjectCell("Inser value"));  //O(1)
+            iterator.Iterate(singlyLinkedList.Top);
+
+            singlyLinkedList.DeleteAfter(foundCell.Next);
+            iterator.Iterate(singlyLinkedList.Top);
+
+
+
+        }
+    }
+}
