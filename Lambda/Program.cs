f8643e79e88869b3dd81f1c719f772faa191aa95
Artur Galimsky <artur.galimsky@gmail.com>
2021-05-16 23:19:27 +0500

    Factory

---
+++ Lambda/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lambda
+{
+    class Program
+    {
+        delegate int Operation(int x, int y);
+        static void Main(string[] args)
+        {
+            Operation op =(x,y) => x + y;
+            System.Console.WriteLine(op(10,20));
+            System.Console.WriteLine(op(40, 20));
+
+            op = (x,y) => x-y;
+            System.Console.WriteLine(op(10, 20));
+            System.Console.WriteLine(op(40,30));
+            Console.Read();
+        }
+    }
+}
