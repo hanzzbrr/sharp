c27aeb4763ea497620515695f7aa73f554d02ea6
Artur Galimsky <artur.galimsky@gmail.com>
2021-05-22 22:29:06 +0500

    config and signalR

---
+++ ProMVC2_ConfiguringApps/Infrastracture/ContentMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProMVC2_ConfiguringApps.Infrastracture
+{
+    public class ContentMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public ContentMiddleware(RequestDelegate next) => nextDelegate = next;
+
+        public async Task Invoke(HttpContext httpcontext)
+        {
+            if(httpcontext.Request.Path.ToString().ToLower() == "/middleware")
+            {
+                await httpcontext.Response.WriteAsync(
+                    "this if from content middleware", Encoding.UTF8);
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpcontext);
+            }
+        }
+    }
+}
---
+++ ProMVC2_ConfiguringApps/Infrastracture/UptimeService.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace ProMVC2_ConfiguringApps.Infrastracture
+{
+    public class UptimeService
+    {
+        private Stopwatch timer;
+
+        public UptimeService()
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        public long Uptime => timer.ElapsedMilliseconds;
+    }
+}
