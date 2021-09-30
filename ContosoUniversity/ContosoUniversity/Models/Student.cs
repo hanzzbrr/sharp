6acaf9cef0a1ef42059bf5c9755493b3c784d4bf
Artur Galimsky <artur.galimsky@gmail.com>
2021-09-30 19:58:50 +0500

    Microsoft ef core official tutorial

---
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        // navigation property, hold other entities that a related to Student entity
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
