3aaf687f005bf79a052ce73d720d2198b62dccd7
hanzzbrr <artur.galimsky@gmail.com>
2022-12-10 13:17:36 +0600

    Karacuba approach

--- Algorithms/Karacuba/Program.cs
+++ Algorithms/Karacuba/Program.cs
@@ -1,14 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine($"2222 * 4444 is equal to {RecIntMult(2222, 4444)}");
-Console.WriteLine($"1234 * 5768 is equal to {RecIntMult(1234, 5768)}");
-Console.WriteLine($"22 * 57 is equal to {RecIntMult(22, 57)}");
+Console.WriteLine($"2222 * 4444 is equal to {RecIntKacu(2222, 4444)}"); // 9 874 568
+Console.WriteLine($"1234 * 5768 is equal to {RecIntKacu(1234, 5768)}"); // 7 117 712
+Console.WriteLine($"22 * 57 is equal to {RecIntKacu(22, 57)}"); // 1 254
 
 // ab  1234   a = 12, b = 34
 // --  ----
 // cd  4444   c = 44, d = 44
 
-int RecIntMult(int x, int y)
+int RecIntKacu(int x, int y)
 {
     // check exceptions
     int n = CountDigits(x);
@@ -22,17 +22,24 @@ int RecIntMult(int x, int y)
     {
         int a = GetFirstHalf(x, n);
         int b = GetSecondHalf(x, n);
+
         int c = GetFirstHalf(y, n);
         int d = GetSecondHalf(y, n);
 
-        int ac = RecIntMult(a, c);
+        int p = a + b;
+        int q = c + d;
 
-        int ad = RecIntMult(a, d);
-        int bc = RecIntMult(b, c);
+        int ac = RecIntKacu(a, c);
+        int bd = RecIntKacu(b, d);
 
-        int bd = RecIntMult(b, d);
+        //int ad = RecIntKacu(a, d);
+        //int bc = RecIntKacu(b, c);
 
-        var result = (int)Math.Pow(10, n) * ac + (int)Math.Pow(10, n / 2) * (ad + bc) + bd;
+        int pq = RecIntKacu(p, q);
+
+
+        //var result = (int)Math.Pow(10, n) * ac + (int)Math.Pow(10, n / 2) * (ad + bc) + bd;
+        var result = (int)Math.Pow(10, n) * ac + (int)Math.Pow(10, n / 2) * (pq - ac - bd) + bd;
         return result;
     }
 }
@@ -47,12 +54,6 @@ int CountDigits(int n)
     } while (n != 0);
 
     return count;
-}
-
-(int, int) GetHalfDigits(int a, int n)
-{
-    int div = (int)Math.Pow(10, n / 2);
-    return (a / div, a % div);
 }
 
 int GetFirstHalf(int a, int n)
